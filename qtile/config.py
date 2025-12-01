1d9d3705934d3caae08e8e31eeb56520b175d414
Kevin Hernandez <kevinhernem@gmail.com>
2025-12-01 18:51:32 +0900

    feat: add keybind for qutebrowser

--- qtile/config.py
+++ qtile/config.py
@@ -92,7 +92,7 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "b", lazy.spawn("firefox")),
+    Key([mod], "b", lazy.spawn("qutebrowser")),
     Key([mod, "shift"], "f", lazy.spawn("pcmanfm")),
     Key([mod], "d", lazy.spawn("rofi -show drun")),
 ]
